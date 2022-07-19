2793723c6574670029eb754944e69eb4ae6f7be5
Bianca <biancamatos.ads@gmail.com>
2022-07-19 10:04:57 -0300

    Tentativa de jogo da forca

--- Cap05/Lab03/exercicio/forca.py
+++ Cap05/Lab03/exercicio/forca.py
@@ -63,11 +63,14 @@ O   |
 =========''']
 
 class Forca:
-    def __init__(self):
-        pass
+    def __init__(self, palavra):
+        self.palavra = palavra
 
-    def adivinhar(self):
-        pass
+    def adivinhar(self, letra):
+        if letra in self.palavra:
+            return True
+        else:
+            return False
 
     def checar_perdeu(self):
         pass
@@ -87,24 +90,30 @@ def sortear_palavra():
     return bank[random.randint(0, len(bank))].strip()
 
 def main():
+    # Executa o programa
+    n_tentativas = len(board)
+    print(board[0])
+
     # Objeto
     jogo = Forca(sortear_palavra())
 
     # Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
+    for tentativa in range(len(board)):
+        letra = input("Informe uma letra: ")
+        status = jogo.adivinhar(letra)
+        print(status)
 
     # Verifica o status do jogo
     jogo.mostrar_status()
 
     # De acordo com o status, imprime mensagem na tela para o usuário
-    if jogo.checar_ganhou():
-        print('\nParabéns! Você venceu!!')
-    else:
-        print('\nGame over! Você perdeu.')
-        print('A palavra era ' + game.word)
+    #if jogo.checar_ganhou():
+    #    print('\nParabéns! Você venceu!!')
+    #else:
+    #    print('\nGame over! Você perdeu.')
+    #    print('A palavra era ' + game.word)
 
-    print('\nFoi bom jogar com você! Agora vá estudar!\n')
-
-    # Executa o programa
+    #print('\nFoi bom jogar com você! Agora vá estudar!\n')
 
 
 if __name__ == "__main__":
